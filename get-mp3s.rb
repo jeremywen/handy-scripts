cea4a03027c069328f1f4dd7c67215dfefc64c4d
jeremywen <jeremy@jeremywentworth.com>
2011-07-20 01:49:45 -0400

    format

--- get-mp3s.rb
+++ get-mp3s.rb
@@ -6,17 +6,17 @@ require 'net/http'
   'http://www.xlr8r.com/mp3',
   'http://www.thedownplayer.com/'
 ].each { |pagepath|
-	doc = Nokogiri::HTML(open(pagepath))
-	doc.xpath('//a').each do |link|
-	  linkpath = link['href']  
-	  if linkpath.end_with?('.mp3') 
-		  filename = linkpath.split('/')[-1]  
-		  puts linkpath + ' filename ' + filename
-		  
-		  resp = Net::HTTP.get_response(URI.parse(linkpath))
-		  open(filename, 'wb') { |file|
-			file.write(resp.body)
-		  }
-	  end
-	end
+  doc = Nokogiri::HTML(open(pagepath))
+  doc.xpath('//a').each do |link|
+    linkpath = link['href']  
+    if linkpath.end_with?('.mp3') 
+      filename = linkpath.split('/')[-1]  
+      puts linkpath + ' filename ' + filename
+  
+      resp = Net::HTTP.get_response(URI.parse(linkpath))
+      open(filename, 'wb') { |file|
+        file.write(resp.body)
+      }
+  end
+end
 }
